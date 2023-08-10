b7cdf6ee0c9f3f59637d928ddec8c29fbfd5b486
Mhqrzscklaus96 <130859914+Mhqrzscklaus96@users.noreply.github.com>
2023-08-10 01:14:08 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+/* const menuBtn = document.querySelector(".menuBtn")
+const links = document.querySelector(".links")
+
+console.log(menuBtn)
+console.log(links) */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* give  start */
+
+
+const openMenu = document.querySelector('.select-menu')
+const sBtntext = document.querySelector('.sBtn-text')
+const options = document.querySelectorAll('.option')
+const Menu  = document.querySelector('.options')
+const chevBtn = document.querySelector('.fa-chevron-down')
+
+chevBtn.addEventListener('click', function(){
+    Menu.classList.toggle('active')
+  console.log("clicked")
+})
+
+
+
+options.forEach(function(option){
+  option.addEventListener('click' , function() {
+    let selected = option.querySelector('.option-text').textContent
+
+sBtntext.textContent = selected
+Menu.classList.remove('active')
+    console.log(selected)
+
+  })
+})
+
+/* give  ends */
